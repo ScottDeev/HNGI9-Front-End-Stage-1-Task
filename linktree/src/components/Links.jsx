0d4f8db6a67051233e9fa222420035796e9e51a0
ScottDeev <amehelijah99@gmail.com>
2022-10-22 11:41:16 +0100

    created the link component and extracted all information to be used into an array of objects for easy access

---
+++ linktree/src/components/Links.jsx
@@ -0,0 +1,39 @@
+
+const LinksData = [
+  {
+    title: "Twitter Link",
+    link: 'https://twitter.com/ElijahLauren',
+    id: 'btn_twitter'
+  },
+  {
+    title: "Zuri Team",
+    link: 'https://training.zuri.team/',
+    id: 'btn__zuri'
+  },
+  {
+    title: "Zuri Books",
+    link: 'http://books.zuri.team',
+    id: 'books'
+  },
+  {
+    title: "Python Books",
+    link: 'https://books.zuri.team/python-for-beginners?ref_id=ScottDev',
+    id: 'book__python'
+  },
+  {
+    title: "Background Check for Coders",
+    link: 'https://background.zuri.team',
+    id: 'pitch'
+  },
+  {
+    title: "Design Books",
+    link: 'https://books.zuri.team/design-rules',
+    id: 'book__design'
+  }
+  
+]
+export default function Links() {
+  return (
+    <div>Links</div>
+  )
+}
