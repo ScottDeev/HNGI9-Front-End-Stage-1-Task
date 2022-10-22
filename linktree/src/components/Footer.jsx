32474a63febe8380030356c4365a55277cc20afb
ScottDeev <amehelijah99@gmail.com>
2022-10-22 12:58:15 +0100

    finished with the responsiveness on mobile screen

--- linktree/src/components/Footer.jsx
+++ linktree/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import zuriIcon from '../assets/zuri.svg'
 import i4gIcon from '../assets/I4G.svg'
 export default function Footer() {
   return (
-    <footer className='mt-[112px] flex flex-row items-end justify-between border-t border-[#EAECF0] h-[64px]'>
+    <footer className='mt-[112px] flex sm:flex-row flex-col sm:items-end items-center justify-between border-t border-[#EAECF0] sm:h-[64px] h-[150px] sm:pt-[0] pt-[20px]'>
       <div>
         <img src={zuriIcon} alt="zuriIcon" />
       </div>
